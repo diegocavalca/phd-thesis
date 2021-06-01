f71ca962a5b244c0003f5ba03a3d75ca792f3ce2
diegocavalca <diegoluizcavalca@gmail.com>
2021-06-01 19:30:24 -0300

    Remoção de pastas/arquivos desncessários

--- neuralnilm/neuralnilm/config.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function, division
-import os
-import ConfigParser
-
-config = ConfigParser.RawConfigParser()
-filename = os.path.expanduser('~/.neuralnilm')
-config.read(filename)
--- neuralnilm/neuralnilm/consts.py
+++
@@ -1,6 +0,0 @@
-DATA_FOLD_NAMES = (
-    'train', 'unseen_appliances', 'unseen_activations_of_seen_appliances')
-
-COLLECTIONS = [
-    'trained_nets', 'train_scores', 'validation_scores',
-    'disaggregation_scores']
